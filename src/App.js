41a07a9e370ff4c2a91ee1f6a0cbcbca73b157e9
Mustafa Erdoğan <erdogann.mustafaaa@gmail.com>
2022-10-15 21:42:58 +0300

    home page edited.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Navbar/>
     <div className='content'>
     <Routes>
+    <Route exact path="/" element={<Home/>}></Route>
     <Route exact path="/home" element={<Home/>}></Route>
     <Route path='/create' element={<Create/>}></Route>
     </Routes>
